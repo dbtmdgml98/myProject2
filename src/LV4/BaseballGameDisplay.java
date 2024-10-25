5f3871c8e11af123f775adf8aa7ac6dac4a50ad8
dbtmdgml98 <dbtmdgml98@naver.com>
2024-10-25 12:35:12 +0900

    Lv4 아웃 출력 추가

--- src/LV4/BaseballGameDisplay.java
+++ src/LV4/BaseballGameDisplay.java
@@ -3,6 +3,10 @@ package LV4;
 /** 힌트(스트라이크와 볼 갯수)를 출력하는 메소드입니다. */
 public class BaseballGameDisplay {
     public void displayHint(int strike, int ball) {
-        System.out.println(strike + "스트라이크" + ball + "볼");
+        if (strike == 0 && ball == 0) {
+            System.out.println("아웃");
+        } else {
+            System.out.println(strike + "스트라이크" + ball + "볼");
+        }
     }
 }
