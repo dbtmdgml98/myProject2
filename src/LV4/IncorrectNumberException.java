6d6b45599acf17cf3ba78a9fedb4fc9eff66a459
dbtmdgml98 <dbtmdgml98@naver.com>
2024-10-25 11:32:10 +0900

    Lv4 주석 javadoc 형식 추가2

--- src/LV4/IncorrectNumberException.java
+++ src/LV4/IncorrectNumberException.java
@@ -1,5 +1,6 @@
 package LV4;
 
+/** 잘못된 입력을 받았을 때 발생하는 예외를 정의한 메소드입니다. */
 public class IncorrectNumberException extends Exception {
     public IncorrectNumberException() {
         super("올바르지 않은 입력값입니다. 자리수는 3, 4, 5자리 숫자 중에서 선택할 수 있습니다.");
