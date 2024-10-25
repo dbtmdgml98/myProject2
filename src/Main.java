1d1943889c7ba2316b59f4eee4bcda66f2dd7be5
dbtmdgml98 <dbtmdgml98@naver.com>
2024-10-25 11:58:04 +0900

    Lv4 허상 주석 및 경고 수정

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,9 @@ public class Main {
     public static void main(String[] args) {
         BaseballGame game = new BaseballGame();  // 객체 생성과 동시에 랜덤정답 생김!
 
-        /** 게임 횟수 변수 */
-        int times = 0;
-
-        /** do~while문 나오기 위한 조건 변수 */
-        int checkout = 0;
-
-        /** 자릿수 설정 변수 */
-        int digits = 0;
+        int times = 0;  // 게임 횟수 변수
+        int checkout = 0;   // do~while문 나오기 위한 조건 변수
+        int digits = 0; // 자릿수 설정 변수
 
         do {
             System.out.println("환영합니다! 원하시는 번호를 입력해주세요");
@@ -23,50 +18,42 @@ public class Main {
             int selectNumber = sc.nextInt();
 
             switch (selectNumber) {
-                case 0:
+                case 0 :
                     System.out.println("설정하고자 하는 자리수를 입력하세요.");
                     digits = sc.nextInt();
 
                     System.out.println(digits + "자리수 난이도로 설정되었습니다.");
 
-                case 1:
-                    /**
-                     * 새로운 정답 랜덤값을 만듭니다.
-                     * 실행 중에 예외가 발생하면 핸들링하여 예외문구를 출력합니다.
-                     */
+                case 1 :
                     try {
-                        game.makeAnswerArray(digits);
-                    } catch (IncorrectNumberException e) {
-                        System.out.println(e.getMessage());
+                        game.makeAnswerArray(digits);   // 새로운 정답 랜덤값을 만든다.
+                    } catch (IncorrectNumberException e) {  // 3,4,5 외의 숫자를 입력하면 예외 발생하고
+                        System.out.println(e.getMessage()); // 예외 메세지 출력
                         break;
                     }
 
                     System.out.println("< 게임을 시작합니다. >");
 
-                    /**
-                     * 실행 중에 예외가 발생하면 핸들링하여 예외문구를 출력합니다.
-                     * 예외가 발생했으면 다시 처음부터 시작합니다.
-                     */
                     int k = 0;
                     do {
                         try {
-                            game.play();
+                            game.play();    // play()를 실행하다가
                             times++;
                             break;
-                        } catch (IllegalArgumentException e) {
-                            System.out.println(e.getMessage());
+                        } catch (IllegalArgumentException e) {  // 예외가 발생하면
+                            System.out.println(e.getMessage()); // 예외메세지 출력
                             k++;
                         }
-                    } while (k != 0);
+                    } while (k != 0);   //예외가 발생하면 다시 처음부터 play()를 실행한다.
                     break;
-                case 2:
+                case 2 :
                     System.out.println("< 게임 기록 보기 >");
 
                     for (int i = 0; i < times; i++) {
                         System.out.println((i+1) + "번째 게임 : 시도 횟수 - " + game.getTryCountList().get(i));
                     }
                     break;
-                case 3:
+                case 3 :
                     System.out.println("< 숫자 야구 게임을 종료합니다 >");
                     checkout++;
                     break;
