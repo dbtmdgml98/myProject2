c04b5326606d49dd352d9ad6a2e48da1945e6dc3
dbtmdgml98 <dbtmdgml98@naver.com>
2024-10-25 12:09:46 +0900

    Lv4 주석 javadoc 형식 추가4

--- src/LV4/BaseballGameDisplay.java
+++ src/LV4/BaseballGameDisplay.java
@@ -1,5 +1,6 @@
 package LV4;
 
+/** 힌트(스트라이크와 볼 갯수)를 출력하는 메소드입니다. */
 public class BaseballGameDisplay {
     public void displayHint(int strike, int ball) {
         System.out.println(strike + "스트라이크" + ball + "볼");
